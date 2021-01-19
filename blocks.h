5c4794180d684d28996e349bc824ef221518c1f0
James Monk <james@jamesmonk.me>
2021-01-19 21:12:21 +0100

    Statuscmd patch applied. https://gist.github.com/IGeraGera/e4a5583b91b3eec2e81fdceb44dea717 Edit to gitignore to save custom module layout/includes, also inclusion of my custom modules.

---
+++ blocks.h
@@ -0,0 +1,17 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+  /*Icon*/  /*Command*/         /*Update Interval*/   /*Update Signal*/
+  {"",      "status-weather",   3600,                 0},
+  {"",      "status-mail",      0,                    3},
+  {"",      "status-rss",       0,                    4},
+  {"",      "status-music",     0,                    1},
+  {"",      "status-updates",   7200,                 2},
+  {"",      "status-volume",    0,                    5},
+  {"",      "status-internet",  60,                   7},
+  {"",      "status-battery",   60,                   6},
+  {"",      "status-time",      60,                   8},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = "\0";
+static unsigned int delimLen = 5;
