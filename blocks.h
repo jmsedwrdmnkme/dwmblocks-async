e7a2233ff19d2dee620f0a218e3a0f17643e7874
James Monk <james@jamesmonk.me>
2021-10-24 14:20:13 +0200

    Update of status bin location

--- blocks.h
+++ blocks.h
@@ -1,18 +1,18 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
   /*Icon*/  /*Command*/         /*Update Interval*/   /*Update Signal*/
-  {"",      "status-weather",   3600,                 9},
-  {"",      "status-rss",       60,                   4},
-  {"",      "status-mail",      60,                   3},
-  {"",      "status-updates",   3600,                 2},
-  {"",      "status-music",     0,                    1},
-  {"",      "status-bluetooth", 60,                   10},
-  {"",      "status-internet",  60,                   7},
-  {"",      "status-mic",       0,                    11},
-  {"",      "status-volume",    0,                    5},
-  {"",      "status-battery",   60,                   6},
-  {"",      "status-date",      60,                   12},
-  {"",      "status-time",      60,                   8},
+  {"",      "~/.local/bin/status/status-weather",   3600,                 9},
+  {"",      "~/.local/bin/status/status-rss",       60,                   4},
+  {"",      "~/.local/bin/status/status-mail",      60,                   3},
+  {"",      "~/.local/bin/status/status-updates",   3600,                 2},
+  {"",      "~/.local/bin/status/status-music",     0,                    1},
+  {"",      "~/.local/bin/status/status-bluetooth", 60,                   10},
+  {"",      "~/.local/bin/status/status-internet",  60,                   7},
+  {"",      "~/.local/bin/status/status-mic",       0,                    11},
+  {"",      "~/.local/bin/status/status-volume",    0,                    5},
+  {"",      "~/.local/bin/status/status-battery",   60,                   6},
+  {"",      "~/.local/bin/status/status-date",      60,                   12},
+  {"",      "~/.local/bin/status/status-time",      60,                   8},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
